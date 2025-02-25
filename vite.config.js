d4b0d838d30acb4067e82db4ac1da9657e36f2de
Johnny Pena <jpenaguitarist@gmaio.com>
2025-02-24 20:15:39 -0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/seating-chart-engine", // Replace with your repository name
+  base: '/seating-chart-engine/', // Replace with your repository name
   plugins: [react()],
 });
