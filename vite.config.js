e2d1bd086f70fbc1855b1d7a6e2c81d0784fd559
Johnny Pena <jpenaguitarist@gmaio.com>
2025-03-05 10:31:44 -0600

    Prepare for deployment to Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/seating-chart-engine/' : '/', // Conditionally set the base URL
+  base: '/', // Set the base URL to root
   plugins: [react()],
 });
