789c2ea7624a712f64d6e1580093f2f325a75205
Johnny Pena <jpenaguitarist@gmaio.com>
2025-03-07 10:43:14 -0600

    Update login component to display alert messages

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,6 +5,7 @@ import { useAuth } from '../context/AuthContext';
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [alertMessage, setAlertMessage] = useState(''); // State for alert message
   const navigate = useNavigate();
   const { login } = useAuth();
 
@@ -26,16 +27,18 @@ function Login() {
         navigate('/seatingChartHomePage');
       } else {
         console.error('Login failed:', data.message);
+        setAlertMessage(`Login failed: ${data.message}`); // Update alert message
       }
     } catch (error) {
       console.error('Error during login:', error);
+      setAlertMessage('Error during login. Please try again later.'); // Update alert message
     }
   };
 
   return (
     <div className="hero bg-base-200 min-h-screen flex items-center justify-center">
       <div className="flex flex-col items-center">
-        <h1 className="text-xl font-bold mb-16 sm:text-3xl">Essential Areas Seating Chart App</h1>
+        <h1 className="text-xl font-bold mb-16 sm:text-3xl">Seating Chart Ninja</h1>
         <h2 className="text-l font-bold mb-4 sm:text-xl">Enter Login Info:</h2>
         <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <label className="input input-bordered flex items-center gap-2">
@@ -60,6 +63,14 @@ function Login() {
             Login
           </button>
         </form>
+        {alertMessage && (
+          <div role="alert" className="alert alert-warning mt-4">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+            </svg>
+            <span>{alertMessage}</span>
+          </div>
+        )}
       </div>
     </div>
   );
